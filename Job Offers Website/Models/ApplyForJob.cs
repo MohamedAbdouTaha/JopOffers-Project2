79c3d7bc969ee09bda69ac5fe43b58928a47b029
mashaly <medo.mashaly.com>
2021-12-11 19:08:29 +0200

    jop offers project

---
+++ Job Offers Website/Models/ApplyForJob.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace Job_Offers_Website.Models
+{
+    public class ApplyForJob
+    {
+        public int Id { get; set; }
+        [Display(Name ="الرساله")]
+        public string Message { get; set; }
+        [Display(Name = "تاريخ التقدم")]
+        public DateTime ApplyDate { get; set; }
+        public int JobId { get; set; }
+        public string UserId { get; set; }
+
+        public virtual Job job { get; set; }
+        public virtual ApplicationUser user { get; set; }
+
+    }
+}
---
+++ Job Offers Website/Models/ContactModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Job_Offers_Website.Models
+{
+    public class ContactModel
+    {
+        [Required]
+        [Display(Name ="الأسم")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "البريد الالكتروني")]
+        public string Email { get; set; }
+        [Required]
+        [AllowHtml]
+        [Display(Name = "الموضوع")]
+        public string Subject { get; set; }
+        [Required]
+        [Display(Name = "الرساله")]
+        public string Message { get; set; }
+    }
+}
